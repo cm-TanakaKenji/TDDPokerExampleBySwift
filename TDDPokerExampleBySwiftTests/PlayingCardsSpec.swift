50aff75c1c8536a2dac7900ef678bec48a96bc95
tanaka.kenji <tanaka.kenji@classmethod.jp>
2017-12-23 22:10:09 +0900

    Let's start TDD.

---
+++ TDDPokerExampleBySwiftTests/PlayingCardsSpec.swift
@@ -0,0 +1,18 @@
+//
+//  PlayingCardsSpec.swift
+//  TDDPokerExampleBySwiftTests
+//
+//  Created by tanaka.kenji on 2017/12/23.
+//  Copyright © 2017年 tanaka.kenji. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import TDDPokerExampleBySwift
+
+class PlayingCardsSpec: QuickSpec {
+    override func spec() {
+        
+    }
+}
